b5f1f2472e0c0aff688bfb5ee7174d1dc08278a7
Glenn Downing <downing@cs.utexas.edu>
2016-08-28 22:32:42 -0500

    another commit

--- projects/collatz/RunCollatz.py
+++ projects/collatz/RunCollatz.py
@@ -20,11 +20,3 @@ from Collatz import collatz_solve
 
 if __name__ == "__main__" :
     collatz_solve(sys.stdin, sys.stdout)
-
-""" #pragma: no cover
-% RunCollatz.py < RunCollatz.in > RunCollatz.out
-
-
-% pydoc3 -w Collatz
-# That creates the file Collatz.html
-"""
