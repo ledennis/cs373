f6252ca9279a7c5f2f45ab3edfdce36ee05308a8
Glenn Downing <downing@cs.utexas.edu>
2016-09-24 23:05:13 -0500

    another commit

--- exercises/RMSE.py
+++ exercises/RMSE.py
@@ -16,46 +16,65 @@
 # http://docs.scipy.org/doc/numpy/reference/generated/numpy.square.html
 # http://docs.scipy.org/doc/numpy/reference/generated/numpy.subtract.html
 
-from functools import reduce
-from math      import sqrt
-from numpy     import mean, sqrt, square, subtract
+from math  import sqrt
+from numpy import mean, sqrt, square, subtract
 
-def rmse_range_for (a, p) :
-    v = 0
+def rmse_range_for (a, p, v) :
+    if not a :
+        return v
     for i in range(len(a)) :
         v += (a[i] - p[i]) ** 2
     return sqrt(v / len(a))
 
-def rmse_zip_for (a, p) :
+def rmse_iterator (a, p, v) :
+    if not a :
+        return v
+    ai = iter(a)
+    pi = iter(p)
+    try :
+        while True :
+            v += (next(ai) - next(pi)) ** 2
+    except StopIteration :
+        pass
+    return sqrt(v / len(a))
+
+def rmse_map_sum (a, p, v) :
+    if not a :
+        return v
+    v = sum(map(lambda x, y : (x - y) ** 2, a, p))
+    return sqrt(v / len(a))
+
+def rmse_zip_for (a, p, v) :
+    if not a :
+        return v
     z = zip(a, p)
     v = 0
     for x, y in z :
         v += (x - y) ** 2
     return sqrt(v / len(a))
 
-def rmse_zip_reduce (a, p) :
-    z = zip(a, p)
-    v = reduce(lambda u, w : u + (w[0] - w[1]) ** 2, z, 0)
-    return sqrt(v / len(a))
-
-def rmse_zip_map_sum (a, p) :
+def rmse_zip_map_sum (a, p, v) :
+    if not a :
+        return v
     z = zip(a, p)
     v = sum(map(lambda u : (u[0] - u[1]) ** 2, z))
     return sqrt(v / len(a))
 
-def rmse_zip_list_sum (a, p) :
+def rmse_zip_list_sum (a, p, v) :
+    if not a :
+        return v
     z = zip(a, p)
     v = sum([(x - y) ** 2 for x, y in z])
     return sqrt(v / len(a))
 
-def rmse_zip_generator_sum (a, p) :
+def rmse_zip_generator_sum (a, p, v) :
+    if not a :
+        return v
     z = zip(a, p)
     v = sum((x - y) ** 2 for x, y in z)
     return sqrt(v / len(a))
 
-def rmse_map_sum (a, p) :
-    v = sum(map(lambda x, y : (x - y) ** 2, a, p))
-    return sqrt(v / len(a))
-
-def rmse_numpy (a, p) :
+def rmse_numpy (a, p, v) :
+    if not a :
+        return v
     return sqrt(mean(square(subtract(a, p))))
