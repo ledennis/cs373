c0c91693358e3c28f7628e34e3b7b926192c3106
Glenn Downing <downing@cs.utexas.edu>
2016-11-15 08:21:37 -0600

    another commit

--- notes/11-14.java
+++ notes/11-14.java
@@ -1,53 +1,37 @@
-/*
-In Java, what are the consequences of an abstract method?
-    1. derived class must define or become abstract
-    2. base class must be abstract
-    3. definition in the base becomes prohibited
-*/
+abstract class A {
+    public final    void f (int) {} // child can not override
+    public          void g (int) {} // child can     override
+    public abstract void h (int);   // child must    override
 
-class A {
-    public void f (int) {}}
-
-class B extends A {
-    public void f (String) {}}
-
-class T {
-    public static void main (...) {
-        B x = new B;
-        x.f(2);}}                   // A.f
+A x;
+if (...)
+    x = new B;
+else
+    x = new C;
+x.f(2);
 
 /*
-Lookup differs between C++ and Java.
-In C++ the name is being looked up. In Java the signature is being looked up.
-The C++ way is better. It reduces the chance of a logic error.
+Java does not exhibit dynamic binding in the following:
+    final   method
+    private method
+    static  method
+    final   class
 */
 
-class A {
-    public void f (long) {}}
+class Lazy {
+    static Lazy l;
 
-class B extends A {
-    public void f (int) {}}
+    private Lazy () {}
 
-class T {
-    public static void main (...) {
-        A x;
-        if (...)
-            x = new B();
-        else
-            x = new C();
-        x.f(2);}}
+    public static Lazy only () {
+        if (l == null)
+            l = new Lazy();
+        return l;}}
 
-/*
-Dynamic binding is implemented via virtual function tables.
-*/
+class Eager {
+    static Eager e = new Eager;
 
-class A {
-    public final    void f (int) {}
-    public          void g (int) {} // avoid
-    public abstract void h (int);
+    private Eager () {}
 
-/*
-Changing f's signature either continues to work or causes a compilation error.
-Changing g's signature can introduce a logic       error, because the derived classes are no longer overriding it.
-Changing h's signature causes        a compilation error, because the derived classes are no longer overriding it.
-*/
+    public static eager only () {
+        return e;}}
